36b930fe4f98e9f965945b83d64a291a70921648
emmanuel <kipronofb@gmail.com>
2019-12-09 06:15:40 +0300

    commit

--- src/PaymentOptions.js
+++ src/PaymentOptions.js
@@ -130,7 +130,8 @@ class PaymentOptions extends React.Component {
                         </Accordion>
 
         return(
-            <div className="payment-options" >
+            <div className="payments-page" >
+                <div className="payment-options">
                 <div className="p-option">
                 <Card  id="mpesa" >
                 <Card.Title id="mpesa">Mpesa</Card.Title>
@@ -159,6 +160,20 @@ class PaymentOptions extends React.Component {
                 </Card>
                     
                 
+                </div>
+                </div>
+                <div className="userprofile">
+                <Card style={{ width: '20rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                </Card>
                 </div>
             </div>
         )
