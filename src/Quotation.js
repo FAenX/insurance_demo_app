8019ce56d7219ecb5ef52d924c4fa4f851ad282b
emmanuel <kipronofb@gmail.com>
2019-12-09 05:43:51 +0300

    commit

--- src/Quotation.js
+++ src/Quotation.js
@@ -25,13 +25,14 @@ class Quotation extends React.Component {
         return(
             <div className="quotation">
             <Card >
-                <h1>{this.props.data.quotation.cover}</h1>
+                <h1>Quotation</h1>
             <ListGroup variant="flush">
                 <ListGroup.Item>Email: {this.props.data.quotation.email} </ListGroup.Item>
                 <ListGroup.Item>REG Number:  {this.props.data.quotation.regNo} </ListGroup.Item>
                 <ListGroup.Item>Vehicle value:  {this.props.data.quotation.carValue} </ListGroup.Item>
                 <ListGroup.Item>Vehicle Capacity:  {this.props.data.quotation.tonnes} </ListGroup.Item>
                 <ListGroup.Item>Premium KSH: {this.props.data.quotation.premium}</ListGroup.Item>
+                <ListGroup.Item>Cover: {this.props.data.quotation.cover}</ListGroup.Item>
             </ListGroup>
             </Card>
             <LoaderButton variant="primary" type="submit" isLoading={this.state.isLoading} onClick={this.handleSubmit}>
