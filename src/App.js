bfc889ddf172a5f580ad451b4f48a3fbf678045d
emmanuel <kipronofb@gmail.com>
2019-12-18 00:47:22 +0300

    covers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Rates from "./Rates"
 import Quotation from "./Quotation"
 import PaymentOptions from "./PaymentOptions"
 import Mpesa from "./Mpesa"
+import Covers from "./Covers";
 import {Container} from "react-bootstrap";
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
@@ -12,15 +13,27 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-     quotation: "",
-     chosenPaymentOption: ""
+      chosenProduct: null,
+      quotation: "",
+      chosenPaymentOption: ""
     }
   }
   
   componentDidMount() {
+    this.setState({
+      chosenProduct: null
+    })
     
   }
 
+  handleChosenProduct = (data)=>{
+    console.log(data)
+    this.setState({
+      chosenProduct: data,
+    })
+  }
+
+  // handle response from quotation request
   handleQuotationRequest =(data)=>{
     console.log(data)
     this.setState({
@@ -29,6 +42,7 @@ class App extends React.Component {
 
   }
 
+  // handle chosen payment method
   handlePayment = (data) => {
     console.log(data)
     this.setState({
@@ -44,7 +58,8 @@ class App extends React.Component {
     <Container className="App">
      
       <Switch>
-          <Route exact path='/' render = {(props) => <Rates {...props} handleRequest={this.handleQuotationRequest}/>}/>
+          <Route exact path='/' render = {(props) => <Covers {...props} chosenProduct={this.handleChosenProduct}/>}/>
+          <Route exact path='/rates' render = {(props) => <Rates {...props} chosenProduct= {this.state.chosenProduct} handleRequest={this.handleQuotationRequest}/>}/>
           <Route exact path='/quotation' render = {(props) => <Quotation {...props} data={this.state}/>}/>
           <Route exact path='/payment-options' render = {(props) => <PaymentOptions {...props} data={this.state} paymentOption={this.handlePayment}/>} />
           <Route exact path='/mpesa' render = {(props) => <Mpesa {...props} data={this.state} />}/>
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,5 +3,5 @@ module.exports = function(app) {
 
   
   app.use(proxy("/api/v1/**", 
-  { target: "http://kiprono123456.pythonanywhere.com/", changeOrigin: true, "secure": false, "logLevel": "debug"}));
+  { target: "http://127.0.0.1:8000/", changeOrigin: true, "secure": false, "logLevel": "debug"}));
 };
