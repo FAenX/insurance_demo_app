b706f76dbb91c9bd331653fb2c0325bed7e727f1
emmanuel <kipronofb@gmail.com>
2019-12-18 05:28:03 +0300

    commit

--- src/PaymentOptions.js
+++ src/PaymentOptions.js
@@ -195,19 +195,7 @@ class PaymentOptions extends React.Component {
                 </div>
                 </div>
                 </div>
-                <div className="userprofile">
-                <Card style={{ width: '20rem' }}>
-                <div><img alt="pp" src={user} /></div>
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-                </Card>
-                </div>
+                
             </div>
         )
     }
