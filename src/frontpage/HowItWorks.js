83ae373a4a36cc51fd4ca81b964fc0e83c5669a4
emmanuel <kipronofb@gmail.com>
2019-12-22 16:47:17 +0300

    commit

--- src/frontpage/HowItWorks.js
+++ src/frontpage/HowItWorks.js
@@ -1,5 +1,8 @@
 import React from "react"
-import img from "../assets/images/cover_bg.svg"
+import selectImg from "../assets/images/cover_bg.svg"
+import quotationImg from "../assets/images/office.svg"
+import doneImg from "../assets/images/done.svg"
+import {Button} from "react-bootstrap"
 
 
 class HowItWorks extends React.Component{
@@ -8,7 +11,7 @@ class HowItWorks extends React.Component{
             <div className="how-it-works">
                 <div className="sub-header"><h1>How does it work?</h1></div>
                 <div className="step">
-                    <div><img alt="" src={img}/></div>
+                    <div><img alt="" src={selectImg}/></div>
                     <div>
                         <div className="step-header">Select motor insurance cover</div>
                         <div>
@@ -17,8 +20,27 @@ We support both transactional email services (Mailgun, Mailjet, Mandrill, Sendin
                         </div>
                     </div>
                 </div>
-                <div className="step"></div>
-                <div className="step"></div>
+                <div className="step">
+                    <div><img alt="" src={quotationImg}/></div>
+                    <div>
+                        <div className="step-header">Request Quotation, Accept and Select Payment Option</div>
+                        <div>
+                        Choose from a wide variety of email services.
+We support both transactional email services (Mailgun, Mailjet, Mandrill, SendinBlue, SendGrid, Amazon SES and Postmark) and personal email services (AOL, Gmail, FastMail, iCloud, Mail.ru, Outlook, Yahoo, Yandex and Zoho).
+                        </div>
+                    </div>
+                </div>
+                <div className="step">
+                    <div><img alt="" src={doneImg}/></div>
+                    <div>
+                        <div className="step-header"> You are Done! Download Cover</div>
+                        <div>
+                        Choose from a wide variety of email services.
+We support both transactional email services (Mailgun, Mailjet, Mandrill, SendinBlue, SendGrid, Amazon SES and Postmark) and personal email services (AOL, Gmail, FastMail, iCloud, Mail.ru, Outlook, Yahoo, Yandex and Zoho).
+                        </div>
+                    </div>
+                </div>
+                <div><Button variant="warning">Get started</Button></div>
             </div>
         )
     }
