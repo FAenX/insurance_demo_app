e7af97577754bed422ca0eac800a969d574beb3d
emmanuel <kipronofb@gmail.com>
2019-12-26 18:46:55 +0300

    commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,6 +3,6 @@ const proxy = require("http-proxy-middleware");
 
   module.exports = function(app) {  
     app.use(proxy("/api/v1/**", 
-    { target: "http://kiprono123456.pythonanywhere.com/", changeOrigin: true, "secure": false, "logLevel": "debug"}));
+    { target: "http://127.0.0.1:8000/", changeOrigin: true, "secure": false, "logLevel": "debug"}));
   };
 
