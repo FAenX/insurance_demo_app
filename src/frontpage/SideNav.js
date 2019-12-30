01c85fe4241a9eefdfb17820c2f3c19a56763ee7
emmanuel <kipronofb@gmail.com>
2019-12-30 12:51:06 +0300

    add social media at the end of menu

--- src/frontpage/SideNav.js
+++ src/frontpage/SideNav.js
@@ -5,6 +5,7 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import SocialMedia from "./SocialMedia"
 
 class SideBar extends React.Component {
 
@@ -47,46 +48,48 @@ class SideBar extends React.Component {
             onClick={this.props.toggleDrawer(false)} 
             onKeyDown={this.props.toggleDrawer(false)}
             >
-                    <List>               
+                    <List>   
+                        <hr className="devider"></hr>            
                         <ListItem button  id="home" onClick={this.handleRedirectOnClick}>                       
                             <ListItemText id="home" primary="Home"/>
                         </ListItem>
-                        <Divider />
+                        <hr className="devider"></hr>
                         <ListItem button id="covers" onClick={this.handleRedirectOnClick}>                       
                             <ListItemText  id="covers" primary="Insurance Covers"/>
                         </ListItem>
-                        <Divider />
+                        <hr className="devider"></hr>
                         <ListItem button id="claim" onClick={this.handleRedirectOnClick}>                       
                             <ListItemText  id="claim" primary="Claim"/>
                         </ListItem>
-                        <Divider />
+                        <hr className="devider"></hr>
                         <ListItem button id="whoarewe" onClick={this.handleRedirectOnClick}>                       
                             <ListItemText  id="whoarewe" primary="Who are we"/>
                         </ListItem>
-                        <Divider /> 
+                        <hr className="devider"></hr>
                         <ListItem button id="signout" onClick={this.handleRedirectOnClick}>                        
                             <ListItemText id="info" primary="What you need to know ..."/>
-                        </ListItem> 
-                          
-                        <Divider />                   
+                        </ListItem>                           
+                        <hr className="devider"></hr>            
                         <ListItem button id="signup" onClick={this.handleRedirectOnClick}>                        
                             <ListItemText id="signup" primary="Sign up"/>
                         </ListItem>
-                        <Divider />
+                        <hr className="devider"></hr>
                         <ListItem button id="signin" onClick={this.handleRedirectOnClick}>                        
                             <ListItemText id="signin" primary="Sign in"/>
                         </ListItem>
-                        <Divider />
+                        <hr className="devider"></hr>
                         <ListItem button id="dashboard" onClick={this.handleRedirectOnClick}>                        
                             <ListItemText id="dashboard" primary="Dashboard"/>
                         </ListItem>  
-                        <Divider />                      
+                        <hr className="devider"></hr>                   
                         <ListItem button id="signout" onClick={this.handleRedirectOnClick}>                        
                             <ListItemText id="signout" primary="Sign out"/>
                         </ListItem> 
-                        <Divider />   
+                        <hr className="devider"></hr>
                         </List>
+                        <SocialMedia/>
             </div>
+            
         </SwipeableDrawer>
         
         </div>
