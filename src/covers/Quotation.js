303015680abd04b4846e3d7113ba32b98d06c4e3
emmanuel <kipronofb@gmail.com>
2020-01-06 10:59:35 +0300

    quotation-layout

--- src/covers/Quotation.js
+++ src/covers/Quotation.js
@@ -27,6 +27,7 @@ class Quotation extends React.Component {
         const vehicle = JSON.parse(sessionStorage.getItem("vehicle"))
         const premium = JSON.parse(sessionStorage.getItem("premium"))
         const chosenProduct = JSON.parse(sessionStorage.getItem("chosen_product"))
+        const user = "user"
         return(
             <div className="quotation-page-wrapper">
                 <div className="coversub-cover">                
@@ -35,57 +36,58 @@ class Quotation extends React.Component {
                         <div className="cover-header-text">
                             <h1>{chosenProduct.name}</h1>
                             <h2>
-                                our moto
+                                Motor Insurance Cover
                             </h2>
                         </div>                        
                         <div className="cover-button"><Button variant="contained">Get started</Button></div>
                     </div>                    
                     </div>            
                 </div>
-                <div className="product-body-wrapper">
-                    <div className="product-text-wrapper">
+                <div className="quotation-body-wrapper">
+                   
                         <div className="product-title">
                         {chosenProduct.name}
                         </div>
-                        <div className="product-description-wrapper">
+                        
                             <div className="quotation-wrapper">
-                                <Paper elevation={4}className="quotation" >                                
+                                <Paper elevation={3}className="quotation" >
+                                    <div id="quotation-top"></div>                              
                                     <div id="quotation-header">
-                                        <div>
-                                            <p>
-                                                Quotation
-                                            </p>
-                                        </div>
-                                            <p className=""> 
-                                                Quote ID:
-                                            </p>
-                                        <div></div>                                       
-                                        
+                                        <h2> Hi {user}, we have found 1 plan for your {vehicle.vehicleMake} {vehicle.vehicleModel}</h2>
                                     </div>
                                     <div id="quotation-content">
+                                        <div id="vehicle-details">
+                                        <ListItem button>Vehicle Use:<b> {vehicle.vehicleUse}</b></ListItem>                                        
                                         <Divider />
-                                        <ListItem button>Email: <b>{vehicle.email} </b></ListItem>
+                                        <ListItem button>Vehicel Make: <b>{vehicle.vehicleMake}</b></ListItem>
                                         <Divider />
+                                        <ListItem button>Vehicel Model: <b>{vehicle.vehicleModel}</b></ListItem>
+                                        <Divider />                                        
                                         <ListItem button>REG Number:  <b>{vehicle.regNo}</b> </ListItem>
                                         <Divider />
                                         <ListItem button>Vehicle value:  <b>{vehicle.vehicleValue}</b> </ListItem>
                                         <Divider />
                                         <ListItem button>Vehicle Capacity:  <b>{vehicle.tonnes}</b> </ListItem>
+                                        </div>
+                                        <div id="insurance-details">                                       
+                                        
+                                        <ListItem button>Cover: <b>{vehicle.cover}</b></ListItem> 
+                                        <Divider /> 
+                                        <ListItem button>Cover to start from: <b>{vehicle.coverStartDate}</b></ListItem>
                                         <Divider />
                                         <ListItem button>Premium KSH: <b>{premium}</b></ListItem>
                                         <Divider />
-                                        <ListItem button>Cover: <b>{vehicle.cover}</b></ListItem>  
-                                    </div>
-                                                             
-                                                                                            
+                                        
+                                        </div> 
+                                    </div>                                                
                                 </Paper>
                             <LoaderButton variant="primary" type="submit" isLoading={this.state.isLoading} onClick={this.handleSubmit}>
                             Accept
                             </LoaderButton>
                             </div>
 
-                        </div>
-                    </div>
+                      
+                   
                 </div>
             </div>
                            
