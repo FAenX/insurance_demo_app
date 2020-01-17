424e1c14a6ebb7889f71876eac51dc28f6f62a8f
emmanuel <kipronofb@gmail.com>
2020-01-17 17:20:43 +0300

    form drawer

--- src/utils/MotorInsuranceQuoteForm.js
+++ src/utils/MotorInsuranceQuoteForm.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {withRouter} from "react-router-dom"
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Drawer from '@material-ui/core/SwipeableDrawer';
 import CloseIcon from '@material-ui/icons/Close';
 import {Button} from "@material-ui/core"
 import VehicleDetails from "./VehicleDetails"
@@ -12,15 +12,13 @@ class MotorInsuranceQuoteForm extends React.Component{
    
     render(){
         return(
-            <SwipeableDrawer 
+            <Drawer 
                 open={this.props.open}
                 onClose={this.props.toggleDrawer(false)}
                 onOpen={this.props.toggleDrawer(true)}
             >
                 <div  
                     className="swipeable-quote-form" 
-                    onClick={this.props.toggleDrawer(false)} 
-                    onKeyDown={this.props.toggleDrawer(false)}
                 >
                     <div className="close-btn-wrapper" onClick={this.props.toggleDrawer(false)}>
                         <div className="close-btn">
@@ -34,7 +32,7 @@ class MotorInsuranceQuoteForm extends React.Component{
                     <div></div>
                        
                 </div>
-            </SwipeableDrawer>
+            </Drawer>
         )
     }
 }
